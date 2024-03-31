4ab88a9b6bc02d48d3c4a5f20dfefe622640d0fc
Thawnder <realthawnder@outlook.fr>
2024-03-31 14:52:06 +0200

    started bonuses

--- srcs/requirements/mariadb/conf/database.sh
+++ srcs/requirements/mariadb/conf/database.sh
@@ -4,11 +4,8 @@ service mysql start
 echo "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE ;" > db.sql
 echo "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD' ;" >> db.sql
 echo "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_USER'@'%' ;" >> db.sql
-# echo "ALTER USER 'root'@'localhost' IDENTIFIED BY $SQL_ROOT_PASSWORD ;" >> db.sql
 echo "FLUSH PRIVILEGES;" >> db.sql
 
 mysql < db.sql
-
-# mysqladmin -u root -p $SQL_ROOT_PASSWORD shutdown
 
 exec mysqld_safe
